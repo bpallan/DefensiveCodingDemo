f48d2b56d293ff9d8916fb7e21343d6163a9e30c
ballan <ballan00@gmail.com>
2019-10-22 23:13:13 -0500

    added demo controller

--- DefensiveCoding.Api/Controllers/DemoController.cs
+++ DefensiveCoding.Api/Controllers/DemoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DefensiveCoding.Api.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DefensiveCoding.Api.Controllers
@@ -13,9 +14,81 @@ namespace DefensiveCoding.Api.Controllers
         // GET api/values
         [HttpGet]
         [Route("success")]
-        public ActionResult<string> Get()
+        public ActionResult<string> Success()
         {
             return "Success!";
         }
+
+        private static int _slowCount = 0;
+
+        [HttpGet]
+        [Route("slow")]
+        public async Task<ActionResult<string>> Slow([FromQuery]int failures)
+        {
+            if (_slowCount < failures)
+            {
+                _slowCount++;
+                await Task.Delay(10000);                
+            }
+            else
+            {
+                _slowCount = 0;
+            }
+
+            return "Slow!";
+        }
+
+        [HttpGet]
+        [Route("unauthorized")]
+        public ActionResult<string> UnAuthorized([FromQuery]string token)
+        {
+            if (token == TokenFactory.GetGoodToken())
+            {
+                return "Authorized!";
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        private static int _errorCount = 0;
+
+        [HttpGet]
+        [Route("error")]
+        public ActionResult<string> Error([FromQuery] int failures)
+        {
+            if (_errorCount < failures)
+            {
+                _errorCount++;
+                return new StatusCodeResult(500);
+            }
+            else
+            {
+                _errorCount = 0;
+            }
+
+            return "Success!";
+        }
+
+        private static int _timeoutCount = 0;
+
+        [HttpGet]
+        [Route("timeout")]
+        public async Task<ActionResult<string>> Timeout([FromQuery] int failures)
+        {
+            if (_timeoutCount < failures)
+            {
+                _timeoutCount++;
+                await Task.Delay(10000);
+                return new StatusCodeResult(408);
+            }
+            else
+            {
+                _timeoutCount = 0;
+            }
+
+            return "Success!";
+        }       
     }
 }
---
+++ DefensiveCoding.Api/Helpers/TokenFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DefensiveCoding.Api.Helpers
+{
+    public static class TokenFactory
+    {
+        public static string GetGoodToken()
+        {
+            return "good_token";
+        }
+
+        public static string GetBadToken()
+        {
+            return "bad_token";
+        }
+    }
+}
