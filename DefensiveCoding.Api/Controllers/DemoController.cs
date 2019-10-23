922db1f81e16badcaf501cbd9b3cb28cc8d54fba
ballan <ballan00@gmail.com>
2019-10-22 22:40:48 -0500

    added initial working solution and api project

---
+++ DefensiveCoding.Api/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DefensiveCoding.Api.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("success")]
+        public ActionResult<string> Get()
+        {
+            return "Success!";
+        }
+    }
+}
