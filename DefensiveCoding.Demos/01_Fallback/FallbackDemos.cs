b4df21154b1dfee5d4a16ba727b9d7438d215f9e
ballan <ballan00@gmail.com>
2019-10-31 00:05:03 -0500

    timeout examples

--- DefensiveCoding.Demos/01_Fallback/FallbackDemos.cs
+++ DefensiveCoding.Demos/01_Fallback/FallbackDemos.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using DefensiveCoding.Demos.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Polly;
 
@@ -12,11 +13,6 @@ namespace DefensiveCoding.Demos._01_Fallback
     [TestClass]
     public class FallbackDemos
     {
-        private static HttpClient _client = new HttpClient()
-        {
-            BaseAddress = new Uri("http://localhost:5001/")
-        };
-
         [TestMethod]
         public async Task HandleAnyException()
         {
@@ -24,7 +20,7 @@ namespace DefensiveCoding.Demos._01_Fallback
                 .Handle<Exception>()
                 .FallbackAsync<string>("Default!");
 
-            var result = await fallBackPolicy.ExecuteAsync(() => _client.GetStringAsync("api/demo/error?failures=1"));
+            var result = await fallBackPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetStringAsync("api/demo/error?failures=1"));
 
             Assert.AreEqual("Default!", result);
         }
@@ -36,7 +32,7 @@ namespace DefensiveCoding.Demos._01_Fallback
                 .HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
                 .FallbackAsync(FallbackAction, OnFallbackAsync);
 
-            var result = await fallBackPolicy.ExecuteAsync(() => _client.GetAsync("api/demo/error?failures=1"));
+            var result = await fallBackPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/error?failures=1"));
             var content = await result.Content.ReadAsStringAsync();
 
             Assert.AreEqual("Default!", content);
@@ -55,17 +51,6 @@ namespace DefensiveCoding.Demos._01_Fallback
                 Content = new StringContent("Default!")
             };
             return Task.FromResult(httpResponseMessage);
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-            ResetDemo().Wait();
-        }
-
-        private static async Task ResetDemo()
-        {            
-            await _client.PostAsync("api/demo/reset", null);
-        }
+        }        
     }
 }
---
+++ DefensiveCoding.Demos/02_Timeout/TimeoutDemos.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DefensiveCoding.Demos.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polly;
+using Polly.Timeout;
+
+namespace DefensiveCoding.Demos._02_Timeout
+{
+    [TestClass]
+    public class TimeoutDemos
+    {
+        [TestMethod]
+        public async Task BasicTimeout()
+        {
+            var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromSeconds(1), TimeoutStrategy.Optimistic); // optimistic is default
+            bool isTimeoutException = false;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            try
+            {
+                await timeoutPolicy.ExecuteAsync(ct => DemoHelper.DemoClient.GetAsync("api/demo/slow?failures=1", ct), CancellationToken.None);
+            }
+            catch (TimeoutRejectedException)
+            {
+                isTimeoutException = true;
+            }
+
+            Assert.IsTrue(isTimeoutException);
+            Assert.IsTrue(sw.ElapsedMilliseconds < 2000);
+        }
+
+        [TestMethod]
+        public async Task OptimisticTimeout_FailsWithoutCancellationToken()
+        {
+            var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromSeconds(1), TimeoutStrategy.Optimistic); // optimistic is default
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string result = await timeoutPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetStringAsync("api/demo/slow?failures=1"));            
+
+            // note that we waited the entire 10 seconds to get here
+            Assert.AreEqual("Slow!", result);
+            Assert.IsTrue(sw.ElapsedMilliseconds > 2000);
+        }
+
+        [TestMethod]
+        public void OptimisticTimeout_FailsForSynchronousCode()
+        {
+            var timeoutPolicy = Policy.Timeout(TimeSpan.FromSeconds(1), TimeoutStrategy.Optimistic); // optimistic is default
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            string result = timeoutPolicy.Execute(() => DemoHelper.DemoClient.GetStringAsync("api/demo/slow?failures=1").Result);
+
+            // note that we waited the entire 10 seconds to get here
+            Assert.AreEqual("Slow!", result);
+            Assert.IsTrue(sw.ElapsedMilliseconds > 2000);
+        }
+
+        [TestMethod]
+        public void PessimisticTimeout_WorksForSynchronousCode()
+        {
+            var timeoutPolicy = Policy.Timeout(TimeSpan.FromSeconds(1), TimeoutStrategy.Pessimistic); // optimistic is default
+            bool isTimeoutException = false;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            try
+            {
+                string result = timeoutPolicy.Execute(() =>
+                    DemoHelper.DemoClient.GetStringAsync("api/demo/slow?failures=1").Result);
+            }
+            catch (TimeoutRejectedException)
+            {
+                isTimeoutException = true;
+            }
+
+            // note that we waited the entire 10 seconds to get here
+            Assert.IsTrue(isTimeoutException);
+            Assert.IsTrue(sw.ElapsedMilliseconds < 2000);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            DemoHelper.Reset();
+        }
+    }
+}
