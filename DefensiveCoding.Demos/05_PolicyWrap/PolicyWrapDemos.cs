cfaffcef27421ec04f4996f3932eba79d163fa41
ballan <ballan00@gmail.com>
2019-11-09 10:51:02 -0600

    fixing issues with policy wrap demo, figured out how to monitor ciruit breaker when IAsync

--- DefensiveCoding.Demos/05_PolicyWrap/PolicyWrapDemos.cs
+++ DefensiveCoding.Demos/05_PolicyWrap/PolicyWrapDemos.cs
@@ -52,7 +52,7 @@ namespace DefensiveCoding.Demos._05_PolicyWrap
                     // optional func or delegate to execute when retry occurs
                     onRetryAsync: OnRetryAsync);
 
-            IAsyncPolicy<HttpResponseMessage> circuitBreakerPolicy = Policy
+            IAsyncPolicy<HttpResponseMessage> circuitBreakerAsyncPolicy = Policy
                 .HandleResult<HttpResponseMessage>(resp => !resp.IsSuccessStatusCode) // consider if you want to ignore 404s
                 .Or<Exception>()
                 // shortening for demo, should be 30 or 60 seconds
@@ -64,21 +64,25 @@ namespace DefensiveCoding.Demos._05_PolicyWrap
                     onReset: (context) =>
                     {
                         Console.WriteLine("Circuit is closed!");
-                    }); 
+                    });
 
+            // pointer to the circuit breaker so we can read/update its state as needed (haven't figured out a better way to do this)
+            ICircuitBreakerPolicy<HttpResponseMessage> circuitBreaker = (ICircuitBreakerPolicy<HttpResponseMessage>)circuitBreakerAsyncPolicy;
+
+            // inner timeout is totally optional and depends on use case.  The outer timeout is the most important for requests with callers waiting
             IAsyncPolicy<HttpResponseMessage> innerTimeoutPolicy = Policy
                 .TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(3), TimeoutStrategy.Pessimistic, onTimeoutAsync: OnTimeoutAsync);  // had to set to pessimistic to get this to work, todo: figure out why
 
             // wrap policies from outer to inner
             var resiliencyPolicy = Policy
-                .WrapAsync<HttpResponseMessage>(fallbackPolicy, outerTimeoutPolicy, retryPolicy, circuitBreakerPolicy, innerTimeoutPolicy);
+                .WrapAsync<HttpResponseMessage>(fallbackPolicy, outerTimeoutPolicy, retryPolicy, circuitBreakerAsyncPolicy, innerTimeoutPolicy);
 
             // verify retry on error
             var response = await resiliencyPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/error?failures=1"));
             Assert.IsTrue(response.IsSuccessStatusCode);
 
             // verify retry on client timeout           
-            response = await retryPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/slow?failures=1", CancellationToken.None));
+            response = await resiliencyPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/slow?failures=1", CancellationToken.None));
             Assert.IsTrue(response.IsSuccessStatusCode);
 
             // verify default if every call times out
@@ -88,8 +92,23 @@ namespace DefensiveCoding.Demos._05_PolicyWrap
             Assert.AreEqual("Default!", result);
 
             // verify circuit broken on errors    
-            response = await retryPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/error", CancellationToken.None));
-            Assert.AreEqual("Default!", result);
+            for (int i = 0; i < 6; i++)
+            {
+                response = await resiliencyPolicy.ExecuteAsync(() =>
+                    DemoHelper.DemoClient.GetAsync("api/demo/error", CancellationToken.None));
+                result = await response.Content.ReadAsStringAsync();
+                Assert.AreEqual("Default!", result);
+            }
+
+            Assert.AreEqual(CircuitState.Open, circuitBreaker.CircuitState);
+
+            // let circuit breaker reset (goes to 1/2 open after 5 seconds)
+            await Task.Delay(5100);
+            Assert.AreEqual(CircuitState.HalfOpen, circuitBreaker.CircuitState);
+
+            // verify circuit breaker is back open
+            await resiliencyPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/success"));
+            Assert.AreEqual(CircuitState.Closed, circuitBreaker.CircuitState);
         }
 
         private async Task OnRetryAsync(DelegateResult<HttpResponseMessage> exception, TimeSpan timeSpan, Context context)
