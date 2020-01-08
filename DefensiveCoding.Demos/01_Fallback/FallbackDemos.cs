4953c034dfdade63cb34a4f90f823d6afb3ab0ee
ballan <ballan00@gmail.com>
2020-01-07 22:43:26 -0600

    added more fallback tests

--- DefensiveCoding.Demos/01_Fallback/FallbackDemos.cs
+++ DefensiveCoding.Demos/01_Fallback/FallbackDemos.cs
@@ -16,7 +16,7 @@ namespace DefensiveCoding.Demos._01_Fallback
     public class FallbackDemos
     {
         /// <summary>
-        /// Demonstrates a very basic fallback in which we just return a default string when any exception is thrown
+        /// Demonstrates a very basic fallback in which we just return a default string when a http exception is thrown
         /// Note:  GetStringAsync exceptions on an error response.  Methods that return a response object do not exception so you must check the result or call EnsureSuccessStatusCode
         /// </summary>
         /// <returns></returns>
@@ -49,23 +49,43 @@ namespace DefensiveCoding.Demos._01_Fallback
             // check for a specific type of exception and error number
             var fallBackPolicy = Policy<string>
                 .Handle<TestException>(ex => ex.Number == 1205)
-                .Fallback<string>("Default!");
+                .Fallback("Default!");
 
-            var result = fallBackPolicy.Execute(QueryData);
+            var result = fallBackPolicy.Execute(() => QueryData(1));
             
             Assert.AreEqual("Default!", result);
+        }
+
+        /// <summary>
+        /// You can chain together multiple exception types to handle
+        /// </summary>
+        [TestMethod]
+        public void HandleMultipleExceptionTypes()
+        {
+            // check for multiple exception types
+            var fallBackPolicy = Policy<string>
+                .Handle<TestException>()
+                .Or<ArgumentException>()
+                .Fallback("Default!");
+
+            var result = fallBackPolicy.Execute(() => QueryData(0));
+            var result2 = fallBackPolicy.Execute(() => QueryData(1));
+
+            Assert.AreEqual("Default!", result);
+            Assert.AreEqual("Default!", result2);
         }
 
         /// <summary>
         /// Demonstrates a basic fallback that is looking at the Response Status Code instead of trying to catch exceptions
         /// Fallback delegates demonstrate logging the fallback and returning a mock response
         /// Other behaviors might be to throw a custom exception, read from cache, etc
+        /// This demo also shows how to use context to store a value used in the handler
         /// </summary>
         /// <returns></returns>
         [TestMethod]
         public async Task HandleHttpErrorResponses()
         {
-            // crate policy
+            // create policy
             var fallBackPolicy = Policy
                 .HandleResult<HttpResponseMessage>(resp => !resp.IsSuccessStatusCode)
                 .FallbackAsync(FallbackAction, PolicyLoggingHelper.LogFallbackAsync);
@@ -74,27 +94,56 @@ namespace DefensiveCoding.Demos._01_Fallback
             var context = new Polly.Context();
             context["DefaultValue"] = "Default2!";
 
-            // execute code wrappe din policy
+            // execute code wrapped in policy
             var response = await fallBackPolicy.ExecuteAsync((ctx) => DemoHelper.DemoClient.GetAsync("api/demo/error"), context);
             var content = await response.Content.ReadAsStringAsync();
 
             Assert.AreEqual("Default2!", content);
         }
 
+        /// <summary>
+        /// Demonstrate handling multiple results in 1 policy using separate handle statements
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task HandleMultipleResponses()
+        {
+            // check for multiple response types (could also just use or conditional and single handle)
+            var fallBackPolicy = Policy
+                .HandleResult<HttpResponseMessage>(resp => resp.StatusCode == HttpStatusCode.InternalServerError)
+                .OrResult(resp => resp.StatusCode == HttpStatusCode.Unauthorized)
+                .FallbackAsync(FallbackAction, PolicyLoggingHelper.LogFallbackAsync);
+
+            var response1 = await fallBackPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/error"));
+            var content1 = await response1.Content.ReadAsStringAsync();
+
+            var response2 = await fallBackPolicy.ExecuteAsync(() => DemoHelper.DemoClient.GetAsync("api/demo/unauthorized"));
+            var content2 = await response2.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("Default!", content1);
+            Assert.AreEqual("Default!", content2);
+        }
+
         // demonstrate returning a mock response
         private Task<HttpResponseMessage> FallbackAction(DelegateResult<HttpResponseMessage> responseToFailedRequest, Context context, CancellationToken cancellationToken)
         {
+            var defaultValue = context.ContainsKey("DefaultValue") ? (string) context["DefaultValue"] : "Default!";
             // mocking a successful response
             // you can pass in responseToFailedRequest.Result.StatusCode if you want to preserve the original error response code
             HttpResponseMessage httpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)        
             {
-                Content = new StringContent((string)context["DefaultValue"])
+                Content = new StringContent(defaultValue)
             };
             return Task.FromResult(httpResponseMessage);
         }
 
-        private string QueryData()
+        private string QueryData(int id)
         {
+            if (id <= 0)
+            {
+                throw new ArgumentException("id is required", nameof(id));
+            }
+            
             throw new TestException()
             {
                 Number = 1205
