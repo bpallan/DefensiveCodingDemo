83f992ea4e99256413a0e6c8a8e9961a6f372f01
ballan <ballan00@gmail.com>
2020-01-15 22:24:16 -0600

    added bulkhead demo

--- DefensiveCoding.Demos/09_Other/BulkHead_Demos.cs
+++ DefensiveCoding.Demos/09_Other/BulkHead_Demos.cs
@@ -1,38 +1,73 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Polly;
+using Polly.Bulkhead;
 
 namespace DefensiveCoding.Demos._09_Other
 {
     [TestClass]
     public class BulkHead_Demos
     {
-        [TestMethod]
-        public void BulkHead_Demo()
+        private List<int> _testData = new List<int>();
+        private ConcurrentBag<int> _processed = new ConcurrentBag<int>();
+        private ConcurrentBag<int> _rejected = new ConcurrentBag<int>();
+
+        public BulkHead_Demos()
         {
+            for (int i = 0; i < 100; i++)
+            {
+                _testData.Add(i);
+            }
+        }
+        
+        /// <summary>
+        /// Demonstrates maxing out the bulkhead so that it rejects executions
+        /// Doesn't check bulkhead status, just catches BulkHeadRejectedException
+        /// You can check available execution and queue slots by checking (see in watch window):
+        ///     bulkHead.BulkheadAvailableCount
+        ///     bulkHead.QueueAvailableCount
+        /// </summary>
+        [TestMethod]
+        public void BulkHeadRejectsExecutionWhenQueueFull()
+        {
+            _processed.Clear();
+            _rejected.Clear();
+            bool exceptionThrown = false;
             var bulkHead = Policy.Bulkhead(1, 5, context =>
             {
-                Console.Out.WriteLine("Bulkhead is full. Execution rejected.");
-            }); // max concurrency of 1, max queue size of 5
-            var testData = new List<int>();
+                var id = context["id"];
+                Console.Out.WriteLine($"Rejected id {id}");
+                _rejected.Add((int)id);
+            }); // max concurrency of 2, max queue size of 5
 
-            for (int i = 0; i < 60; i++)
+            Parallel.ForEach(_testData, id =>
             {
-                testData.Add(i);
-            }
-
-            Parallel.ForEach(testData, currentData =>
-            {
-                bulkHead.Execute(() => SlowFunction(currentData));
+                try
+                {
+                    var context = new Polly.Context {["id"] = id};
+                    bulkHead.Execute((ctx) => SlowFunction(id), context);
+                }
+                catch (BulkheadRejectedException)
+                {
+                    // keep demo running
+                    exceptionThrown = true;
+                }
             });
+            
+            Assert.IsTrue(exceptionThrown);
+            Assert.IsTrue(_processed.Count > 0);
+            Assert.IsTrue(_rejected.Count > 0);
+            Assert.IsTrue(_rejected.Count > _processed.Count); // we will always reject more since method takes 1 second
         }
 
-        private void SlowFunction(int data)
+        private void SlowFunction(int id)
         {
             Task.Delay(1000).Wait();
-            Console.Out.WriteLine($"Received id {data}");
+            Console.Out.WriteLine($"Received id {id}");
+            _processed.Add(id);
         }
     }
 }
